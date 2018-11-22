bb716ffb5cc60bde99c161570c4bd2b705d4da1e
vshtrash <vshtrash@inbox.lv>
2018-11-22 15:24:34 +0200

    Initial

--- src/main/java/com/dmd/entity/lookup/LookupName.java
+++ src/main/java/com/dmd/entity/lookup/LookupName.java
@@ -1,9 +1,15 @@
 package com.dmd.entity.lookup;
 
+
+import com.dmd.tools.json.StringHashMapValueDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class LookupName {
+    @JsonDeserialize(using = StringHashMapValueDeserializer.class)
     String name;
     List <LookupValue> values = new ArrayList<LookupValue>();
 }
